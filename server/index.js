77474935d573b49760a32d7c442ffd954f7f8c06
Codingspy <praveenshinde2022mite@gmail.com>
2025-08-08 18:40:04 +0530

    added dotenv for mongodb atlas

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const http = require('http');
 const { Server } = require('socket.io');
 const mongoose = require('mongoose');
 const cors = require('cors');
+require('dotenv').config();
 
 const app = express();
 const server = http.createServer(app);
@@ -13,7 +14,7 @@ const io = new Server(server, {
 });
 
 // MongoDB connection
-mongoose.connect('mongodb://127.0.0.1:27017/chat_support_mvp', {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }).then(() => console.log('MongoDB Connected'))
