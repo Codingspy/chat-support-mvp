eb9e0b77dfbe6e7bb928cacb618c9b4a0610b6b5
Codingspy <praveenshinde2022mite@gmail.com>
2025-08-08 20:42:49 +0530

    added ticket status and close feature

--- client/src/components/AgentDashboard.vue
+++ client/src/components/AgentDashboard.vue
@@ -3,7 +3,8 @@
     <h2>Agent Dashboard</h2>
     <ul>
       <li v-for="ticket in tickets" :key="ticket.ticketId">
-        {{ ticket.ticketId }} - {{ ticket.user }}
+        {{ ticket.ticketId }} - {{ ticket.user }} - {{ ticket.status }}
+        <button @click="closeTicket(ticket.ticketId)" v-if="ticket.status === 'open'">Close</button>
       </li>
     </ul>
   </div>
@@ -16,8 +17,17 @@ import axios from 'axios'
 const apiUrl = import.meta.env.VUE_APP_API_URL
 const tickets = ref([])
 
-onMounted(async () => {
+const loadTickets = async () => {
   const res = await axios.get(`${apiUrl}/tickets`)
   tickets.value = res.data
+}
+
+const closeTicket = async (ticketId) => {
+  await axios.post(`${apiUrl}/tickets/${ticketId}/close`)
+  loadTickets()
+}
+
+onMounted(() => {
+  loadTickets()
 })
 </script>
